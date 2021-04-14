edab721165d7d7495bf72ce025f4ba80f6379f6b
JaZax <iza.janeczekk@gmail.com>
2021-04-14 20:47:12 +0200

    something

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,6 @@
 const ranges = document.getElementsByClassName('range')
 const colorDiv = document.getElementById('colorPreview')
+const checkBox = document.getElementById('hashCheck')
 
 let red = 100;
 let green = 100;
@@ -23,4 +24,27 @@ for(let i = 0; i < 3; i++){
 
         colorDiv.style.background = `rgb(${red}, ${green}, ${blue})`
     })
+}
+
+colorDiv.addEventListener('click', ()=>{
+
+    console.log(red + ' ' + green + ' ' + blue)
+    console.log(red.toString(16) + ' ' + green.toString(16) + ' ' + blue.toString(16))
+
+    if(checkBox.checked) {
+        textToClipboard (`#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`)
+    }  else {
+        textToClipboard (`${red.toString(16)}${green.toString(16)}${blue.toString(16)}`)
+    } 
+})
+
+// thx https://stackoverflow.com/questions/33855641/copy-output-of-a-javascript-variable-to-the-clipboard
+
+function textToClipboard (text) {
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
 }
